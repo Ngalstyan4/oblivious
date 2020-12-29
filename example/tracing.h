de8390a4972b57b3e4fe2b2d071a2f00efa1b134
Narek Galstyan <narekg@berkeley.edu>
2020-12-29 14:06:12 -0800

    Refactor syscall interface

---
+++ example/tracing.h
@@ -0,0 +1,17 @@
+#ifndef TRACING_H
+#define TRACING_H
+
+enum trace_flags {
+	START = 1 << 0,
+	PAUSE = 1 << 1,
+	RESUME = 1 << 2,
+	END = 1 << 3,
+
+	RECORD = 1 << 4,
+	PREFETCH = 1 << 5,
+	AUTO = 1 << 6,
+};
+
+#define mem_pattern_trace 328
+
+#endif /*TRACING_H*/
--- tracer/tracer.h
+++ tracer/tracer.h
@@ -1,8 +1,7 @@
 #ifndef TRACER_H
 #define TRACER_H
 #include <linux/linkage.h>
-asmlinkage int sys_mem_pattern_trace_start(void);
-asmlinkage int sys_mem_pattern_trace_end(void);
+asmlinkage int sys_mem_pattern_trace(int flags);
 
 #endif /*TRACER_H*/
 
