ad4f7aa42b2580523869d569be1a72fb46ce2631
Narek Galstyan <narekg@berkeley.edu>
2021-01-16 21:14:04 -0800

    Add module to /extra so perf finds symbols for ~/perf report

--- injector/cli.sh
+++ injector/cli.sh
@@ -1,4 +1,10 @@
 # cmd=${1-"trace_init"}
+MOD="mem_pattern_trace"
 cmd=${1-"help"}
 val=${2-"invalid"}
-sudo rmmod mem_pattern_trace.ko ;sudo insmod mem_pattern_trace.ko cmd="$cmd" val="$val"
+sudo rmmod $MOD.ko ;sudo insmod $MOD.ko cmd="$cmd" val="$val"
+
+# perf uses these in "extra" dir to extract symbols in `perf report -g`
+# source` https://stackoverflow.com/questions/44326565/perf-kernel-module-symbols-not-showing-up-in-profiling
+sudo ln -sf `pwd`/$MOD.ko  /lib/modules/`uname -r`/extra/$MOD.ko
+
