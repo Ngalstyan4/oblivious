c8048eb1f0fc85c642b025947644f9d907431c84
Sam Kumar <samkumar99@gmail.com>
2021-02-06 17:08:49 -0800

    Avoid tracking instruction pages
    Avoids issues when tracing first run after boot. To reproduce issue, run "echo 3 | sudo tee /proc/sys/vm/drop_caches" before tracing the program.

--- injector/common.h
+++ injector/common.h
@@ -17,6 +17,27 @@ extern struct dentry *debugfs_root;
 // for coherent post processing
 extern atomic_t metronome;
 
+/* Copied from arch/x86/mm/fault.c. */
+/*
+ * Page fault error code bits:
+ *
+ *   bit 0 ==	 0: no page found	1: protection fault
+ *   bit 1 ==	 0: read access		1: write access
+ *   bit 2 ==	 0: kernel-mode access	1: user-mode access
+ *   bit 3 ==				1: use of reserved bit detected
+ *   bit 4 ==				1: fault was an instruction fetch
+ *   bit 5 ==				1: protection keys block access
+ */
+enum x86_pf_error_code {
+
+	PF_PROT		=		1 << 0,
+	PF_WRITE	=		1 << 1,
+	PF_USER		=		1 << 2,
+	PF_RSVD		=		1 << 3,
+	PF_INSTR	=		1 << 4,
+	PF_PK		=		1 << 5,
+};
+
 #define FILEPATH_LEN 256
 extern const char *TRACE_FILE_FMT;
 
