29f7d545c993b3d9ddc3e770ebbd2720478b6f46
Narek Galstyan <narekg@berkeley.edu>
2021-02-11 15:54:04 -0800

    Add python experiments

---
+++ experiments/python/mem_pattern_trace.py
@@ -0,0 +1,21 @@
+import ctypes
+libc = ctypes.CDLL(None)
+syscall = libc.syscall
+
+TRACE_START = 1 << 0
+TRACE_PAUSE = 1 << 1
+TRACE_RESUME = 1 << 2
+TRACE_END = 1 << 3
+TRACE_RECORD = 1 << 4
+TRACE_PREFETCH = 1 << 5
+TRACE_AUTO = 1 << 6
+KEVICTD_INIT = 1<<7
+KEVICTD_FINI = 1<<8
+
+mem_pattern_trace = 333
+
+
+
+
+
+
