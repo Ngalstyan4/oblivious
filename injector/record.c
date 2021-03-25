1693e2024b779a9c42d4608c5691bfb7ee7ef71d
Narek Galstyan <narekg@berkeley.edu>
2021-03-24 23:58:16 -0700

    Finish/cleanup parallel tracing implementation

--- injector/record.c
+++ injector/record.c
@@ -10,35 +10,19 @@
 #include "record.h"
 #include "mem_pattern_trace.h"
 
+#include <linux/kallsyms.h>
+
+// todo:: get rid of this after kernel recompile
+static void (*flush_tlb_all_p)(void);
+
 #define TRACE_ARRAY_SIZE 1024 * 1024 * 1024 * 1ULL
 #define TRACE_MAX_LEN (TRACE_ARRAY_SIZE / sizeof(void *))
-
-int limit = 200000;
-
-/*
-typedef struct {
-	pid_t process_pid;
-	struct mm_struct *mm;
-	unsigned long *accesses;
-	unsigned long pos;
-	char filepath[FILEPATH_LEN];
-
-	unsigned long microset_size;
-	unsigned long microset_pos;
-	unsigned long *microset;
-} tracing_state;
-static tracing_state trace;
-
-*/
-static void do_unmap_5(pte_t *pte);
-static void do_page_fault_2(struct pt_regs *regs, unsigned long error_code,
-			    unsigned long address, struct task_struct *tsk,
-			    bool *return_early, int magic);
 
 void record_init(struct task_struct *tsk, int flags, unsigned int microset_size)
 {
 	struct trace_recording_state *record = &tsk->obl.record;
 
+	flush_tlb_all_p = (void *)kallsyms_lookup_name("flush_tlb_all");
 	// in case a prvious recording was sigkilled before FINI call
 	record_fini(tsk);
 
@@ -63,41 +47,6 @@ void record_init(struct task_struct *tsk, int flags, unsigned int microset_size)
 	}
 	memset(record->microset, 0x00,
 	       record->microset_size * sizeof(unsigned long));
-
-	set_pointer(5, do_unmap_5);
-	set_pointer(6, do_unmap_5); //<-- for handle_pte_fault
-	set_pointer(2, do_page_fault_2);
-}
-
-void record_init1(struct task_struct *tsk, int flags, unsigned int microset_size)
-{
-	struct trace_recording_state *record = &tsk->obl.record;
-
-	// in case a prvious recording was sigkilled before FINI call
-	//record_fini(tsk);
-
-	memset(record, 0, sizeof(struct trace_recording_state));
-	tsk->obl.flags = flags;
-
-	record->accesses = vmalloc(TRACE_ARRAY_SIZE);
-	if (record->accesses == NULL) {
-		printk(KERN_ERR "Unable to allocate memory for tracing\n");
-		return;
-	}
-
-	record->microset_size = microset_size;
-	record->microset_pos = 0;
-	record->microset =
-		vmalloc(record->microset_size * sizeof(unsigned long));
-	if (record->microset == NULL) {
-		printk(KERN_ERR "Unable to allocate memory for tracing\n");
-		vfree(record->accesses);
-		record->accesses = NULL;
-		return;
-	}
-	memset(record->microset, 0x00,
-	       record->microset_size * sizeof(unsigned long));
-
 }
 
 static void drain_microset();
@@ -109,14 +58,7 @@ bool record_initialized(struct task_struct *tsk)
 
 void record_clone(struct task_struct *p, unsigned long clone_flags)
 {
-
-	record_init1(p, current->obl.flags, current->obl.record.microset_size);
-	//struct task_struct *me = current;
-	//struct task_struct *t = me;
-	//do {
-	//	printk(KERN_ERR "record clone info: %d", t->pid);
-	//}
-	//while_each_thread(me, t);
+	record_init(p, current->obl.flags, current->obl.record.microset_size);
 }
 
 void record_fini(struct task_struct *tsk)
@@ -129,7 +71,8 @@ void record_fini(struct task_struct *tsk)
 		snprintf(trace_filepath, FILEPATH_LEN, RECORD_FILE_FMT,
 			 tsk->comm, thread_ind);
 
-		printk(KERN_ERR "for pid %d writing at %s\n", tsk->pid, trace_filepath);
+		printk(KERN_ERR "for pid %d writing at %s\n", tsk->pid,
+		       trace_filepath);
 		// in case path is too long, truncate;
 		trace_filepath[FILEPATH_LEN - 1] = '\0';
 		down_read(&tsk->mm->mmap_sem);
@@ -148,18 +91,6 @@ void record_fini(struct task_struct *tsk)
 		vfree(record->microset);
 		vfree(record->accesses);
 		// make sure next call to record_initialized() will return false;
-		record->accesses = NULL;
-	}
-}
-
-// in case we faulted while tracing and therefore trace_fini() was never called, record_force_clean can be called
-// upon module cleanup
-void record_force_clean()
-{
-	struct trace_recording_state *record = &current->obl.record;
-	if (record->accesses) {
-		vfree(record->microset);
-		vfree(record->accesses);
 		record->accesses = NULL;
 	}
 }
@@ -213,22 +144,9 @@ static void drain_microset()
 	record->microset_pos = 0;
 }
 
-static void do_unmap_5(pte_t *pte)
-{
-	unsigned long pte_deref_value = native_pte_val(*pte);
-	if (pte_deref_value & SPECIAL_BIT_MASK) {
-		pte_deref_value |= PRESENT_BIT_MASK;
-		pte_deref_value &= ~SPECIAL_BIT_MASK;
-		// todo:: this treats the symptom of a present-marked NULL pointer
-		// find the cause later
-		if ((pte_deref_value & ~PRESENT_BIT_MASK) == 0)
-			pte_deref_value = 0;
-		set_pte(pte, native_make_pte(pte_deref_value));
-	}
-}
-static void do_page_fault_2(struct pt_regs *regs, unsigned long error_code,
-			    unsigned long address, struct task_struct *tsk,
-			    bool *return_early, int magic)
+void record_page_fault_handler(struct pt_regs *regs, unsigned long error_code,
+			       unsigned long address, struct task_struct *tsk,
+			       bool *return_early, int magic)
 {
 
 	struct trace_recording_state *record = &current->obl.record;
@@ -242,6 +160,27 @@ static void do_page_fault_2(struct pt_regs *regs, unsigned long error_code,
 	}
 
 	if (current->obl.flags & TRACE_RECORD && record->pos < TRACE_MAX_LEN) {
+
+		struct vm_area_struct *maybe_stack =
+			find_vma(current->mm, address);
+		if (unlikely(0x800000 ==
+			     maybe_stack->vm_end - maybe_stack->vm_start)) {
+			struct vm_area_struct *prot_page = maybe_stack->vm_prev;
+			/*
+			 * Each mprotect() call explicitly passes r/w/x permissions.
+			 * If a permission is not passed to mprotect(), it must be
+			 * cleared from the VMA.
+			 */
+			if (0x1000 == prot_page->vm_end - prot_page->vm_start &&
+			    !(prot_page->vm_flags &
+			      (VM_READ | VM_WRITE | VM_EXEC))) {
+				/* ok, we found a 8MB vm area that has a single unmapped page at
+				 * the beginning. Let's assume it is a stack page
+				 */
+				return;
+			}
+		}
+
 		down_read(&current->mm->mmap_sem);
 
 		if (record->microset_pos == record->microset_size) {
@@ -258,15 +197,15 @@ static void do_page_fault_2(struct pt_regs *regs, unsigned long error_code,
 		trace_maybe_set_pte(addr2pte(address, current->mm),
 				    return_early);
 
-		get_cpu();
-		count_vm_tlb_event(NR_TLB_LOCAL_FLUSH_ALL);
-		local_flush_tlb();
 		// the following, if used correctly, can trace TLB count.
 		// trace_tlb_flush(TLB_LOCAL_SHOOTDOWN, TLB_FLUSH_ALL);
+		//count_vm_tlb_event(NR_TLB_LOCAL_FLUSH_ALL);
+		get_cpu();
+		flush_tlb_all_p();
+		//local_flush_tlb();
 		put_cpu();
 
 		up_read(&current->mm->mmap_sem);
 	}
 }
-EXPORT_SYMBOL(do_page_fault_2);
 /************************** TRACE RECORDING FOR MEMORY PREFETCHING END ********************************/
--- injector/record.h
+++ injector/record.h
@@ -1,9 +1,12 @@
 #ifndef RECORD_H
 #define RECORD_H
 
-void record_init(struct task_struct *tsk, int flags, unsigned int microset_size);
+void record_init(struct task_struct *tsk, int flags,
+		 unsigned int microset_size);
 bool record_initialized(struct task_struct *tsk);
+void record_page_fault_handler(struct pt_regs *regs, unsigned long error_code,
+			       unsigned long address, struct task_struct *tsk,
+			       bool *return_early, int magic);
 void record_clone(struct task_struct *p, unsigned long clone_flags);
 void record_fini(struct task_struct *tsk);
-void record_force_clean();
 #endif /*RECORD_H*/
